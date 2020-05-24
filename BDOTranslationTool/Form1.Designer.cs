c2902b4f535443dee14d4229581143c2ba68ad7b
Lê Hiếu <51288927+lehieugch68@users.noreply.github.com>
2020-05-24 16:52:43 +0700

    Add files via upload

---
+++ BDOTranslationTool/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace BDOTranslationTool
+{
+    partial class BDOTranslationTool
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.GamePath = new System.Windows.Forms.TextBox();
+            this.Browser = new System.Windows.Forms.Button();
+            this.Log = new System.Windows.Forms.ListBox();
+            this.Install = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.Label();
+            this.buttonDecompress = new System.Windows.Forms.Button();
+            this.linkGithub = new System.Windows.Forms.LinkLabel();
+            this.linkVHG = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 140);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(410, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // GamePath
+            // 
+            this.GamePath.Location = new System.Drawing.Point(108, 24);
+            this.GamePath.Name = "GamePath";
+            this.GamePath.ReadOnly = true;
+            this.GamePath.Size = new System.Drawing.Size(310, 20);
+            this.GamePath.TabIndex = 1;
+            // 
+            // Browser
+            // 
+            this.Browser.Location = new System.Drawing.Point(12, 24);
+            this.Browser.Name = "Browser";
+            this.Browser.Size = new System.Drawing.Size(90, 23);
+            this.Browser.TabIndex = 2;
+            this.Browser.Text = "Chọn thư mục";
+            this.Browser.UseVisualStyleBackColor = true;
+            this.Browser.Click += new System.EventHandler(this.Browser_Click);
+            // 
+            // Log
+            // 
+            this.Log.FormattingEnabled = true;
+            this.Log.Location = new System.Drawing.Point(12, 169);
+            this.Log.Name = "Log";
+            this.Log.Size = new System.Drawing.Size(410, 160);
+            this.Log.TabIndex = 3;
+            // 
+            // Install
+            // 
+            this.Install.Location = new System.Drawing.Point(12, 74);
+            this.Install.Name = "Install";
+            this.Install.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Install.Size = new System.Drawing.Size(202, 30);
+            this.Install.TabIndex = 4;
+            this.Install.Text = "Cài đặt";
+            this.Install.UseVisualStyleBackColor = true;
+            this.Install.Click += new System.EventHandler(this.Install_Click);
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Location = new System.Drawing.Point(12, 124);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(44, 13);
+            this.Status.TabIndex = 6;
+            this.Status.Text = "Chưa rõ";
+            // 
+            // buttonDecompress
+            // 
+            this.buttonDecompress.Location = new System.Drawing.Point(220, 74);
+            this.buttonDecompress.Name = "buttonDecompress";
+            this.buttonDecompress.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonDecompress.Size = new System.Drawing.Size(202, 30);
+            this.buttonDecompress.TabIndex = 8;
+            this.buttonDecompress.Text = "Giải nén";
+            this.buttonDecompress.UseVisualStyleBackColor = true;
+            this.buttonDecompress.Click += new System.EventHandler(this.buttonDecompress_Click);
+            // 
+            // linkGithub
+            // 
+            this.linkGithub.AutoSize = true;
+            this.linkGithub.Location = new System.Drawing.Point(9, 342);
+            this.linkGithub.Name = "linkGithub";
+            this.linkGithub.Size = new System.Drawing.Size(38, 13);
+            this.linkGithub.TabIndex = 9;
+            this.linkGithub.TabStop = true;
+            this.linkGithub.Text = "Github";
+            this.linkGithub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkGithub_LinkClicked);
+            // 
+            // linkVHG
+            // 
+            this.linkVHG.AutoSize = true;
+            this.linkVHG.Location = new System.Drawing.Point(349, 342);
+            this.linkVHG.Name = "linkVHG";
+            this.linkVHG.Size = new System.Drawing.Size(73, 13);
+            this.linkVHG.TabIndex = 10;
+            this.linkVHG.TabStop = true;
+            this.linkVHG.Text = "VietHoaGame";
+            this.linkVHG.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkVHG_LinkClicked);
+            // 
+            // BDOTranslationTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 381);
+            this.Controls.Add(this.linkVHG);
+            this.Controls.Add(this.linkGithub);
+            this.Controls.Add(this.buttonDecompress);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Install);
+            this.Controls.Add(this.Log);
+            this.Controls.Add(this.Browser);
+            this.Controls.Add(this.GamePath);
+            this.Controls.Add(this.progressBar);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(450, 420);
+            this.MinimumSize = new System.Drawing.Size(450, 420);
+            this.Name = "BDOTranslationTool";
+            this.Text = "BDO Translation Tool";
+            this.Load += new System.EventHandler(this.BDOTranslationTool_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox GamePath;
+        private System.Windows.Forms.Button Browser;
+        private System.Windows.Forms.ListBox Log;
+        private System.Windows.Forms.Button Install;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Button buttonDecompress;
+        private System.Windows.Forms.LinkLabel linkGithub;
+        private System.Windows.Forms.LinkLabel linkVHG;
+    }
+}
+
---
+++ BDOTranslationTool/Form1.cs
@@ -0,0 +1,447 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using ICSharpCode.SharpZipLib.Zip.Compression;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace BDOTranslationTool
+{
+    public partial class BDOTranslationTool : Form
+    {
+        string _AppPath = AppDomain.CurrentDomain.BaseDirectory;
+        string _GamePath;
+        bool _Installing = false, _Uninstalling = false, _Decompress = false;
+        public BDOTranslationTool()
+        {
+            InitializeComponent();
+        }
+
+        private void ReportProgress(int percent)
+        {
+            if (percent <= 100)
+            {
+                this.progressBar.BeginInvoke((MethodInvoker)delegate ()
+                {
+                    progressBar.Value = percent;
+                });
+            }
+        }
+
+        private void ReportStatus(string status)
+        {
+            this.Status.BeginInvoke((MethodInvoker)delegate ()
+            {
+                Status.Text = status;
+            });
+        }
+
+        private void Write_Log(string text)
+        {
+            this.Log.BeginInvoke((MethodInvoker)delegate ()
+            {
+                Log.Items.Add(text);
+                Log.SelectedIndex = Log.Items.Count - 1;
+            });
+        }
+
+        private void BDOTranslationTool_Load(object sender, EventArgs e)
+        {
+            string registryPath = @"SOFTWARE\Wow6432Node\BlackDesert_ID";
+            try
+            {
+                _GamePath = Registry.LocalMachine.OpenSubKey(registryPath).GetValue("Path").ToString();
+                GamePath.Text = _GamePath;
+            }
+            catch
+            {
+                MessageBox.Show("Không tìm thấy thư mục cài đặt Black Desert Online!\nVui lòng chọn đường dẫn thủ công.", "Thông báo");
+            }
+        }
+
+        private void Browser_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    _GamePath = fbd.SelectedPath;
+                    GamePath.Text = _GamePath;
+                }
+            }
+        }
+
+        private void Install_Click(object sender, EventArgs e)
+        {
+            if (!_Installing && !_Uninstalling && !_Decompress)
+            {
+                string sourceFile = $"{_GamePath}\\ads\\languagedata_en.loc";
+                string encryptFile = $"{_AppPath}\\languagedata_en.loc";
+                string decryptFile = $"{_AppPath}\\languagedata_en.tsv";
+                string translationFile = $"{_AppPath}\\BDO_Translation.tsv";
+                if (File.Exists(sourceFile))
+                {
+                    _Installing = true;
+                    ReportProgress(0);
+                    Task.Run(() => decrypt(decompress(sourceFile), decryptFile)).GetAwaiter().OnCompleted(() =>
+                    {
+                        if (!_Installing) return;
+                        ReportProgress(25);
+                        Task.Run(() => Replace_Text(decryptFile, translationFile)).GetAwaiter().OnCompleted(() =>
+                        {
+                            if (!_Installing) return;
+                            ReportProgress(50);
+                            Task.Run(() => compress(encrypt(decryptFile), decryptFile)).GetAwaiter().OnCompleted(() =>
+                            {
+                                if (!_Installing) return;
+                                ReportStatus("Đang sao chép");
+                                ReportProgress(75);
+                                Task.Run(() => CopyFile(encryptFile, sourceFile)).GetAwaiter().OnCompleted(() =>
+                                {
+                                    if (!_Installing) return;
+                                    _Installing = false;
+                                    ReportStatus("Cài đặt thành công!");
+                                    ReportProgress(100);
+                                });
+                            });
+                        });
+                    });
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy tệp languagedata_en.loc!", "Thông báo");
+                }
+            }
+
+        }
+
+        private void buttonDecompress_Click(object sender, EventArgs e)
+        {
+            if (!_Installing && !_Uninstalling && !_Decompress)
+            {
+                string sourceFile = $"{_GamePath}\\ads\\languagedata_en.loc";
+                string decryptFile = $"{_AppPath}\\languagedata_en.tsv";
+                string translationFile = $"{_AppPath}\\BDO_Translation.tsv";
+                if (File.Exists(translationFile))
+                {
+                    DialogResult dialogResult = MessageBox.Show("Phát hiện tệp BDO_Translation.tsv\nBạn có chắc muốn ghi đè tệp này?", "Thông báo", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        if (File.Exists(sourceFile))
+                        {
+                            _Decompress = true;
+                            ReportProgress(0);
+                            Task.Run(() => { decrypt(decompress(sourceFile), decryptFile); }).GetAwaiter().OnCompleted(() =>
+                            {
+                                if (!_Decompress) return;
+                                ReportProgress(33);
+                                Task.Run(() => {
+                                    File.WriteAllBytes(translationFile, File.ReadAllBytes(decryptFile));
+                                }).GetAwaiter().OnCompleted(() =>
+                                {
+                                    ReportProgress(67);
+                                    Task.Run(() => { Remove_Duplicate(translationFile); }).GetAwaiter().OnCompleted(() =>
+                                    {
+                                        _Decompress = false;
+                                        ReportStatus("Giải nén thành công!");
+                                        ReportProgress(100);
+                                    });
+                                });
+                            });
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không tìm thấy tệp languagedata_en.loc!", "Thông báo");
+                        }
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void CopyFile (string sourceFile, string destinationFile)
+        {
+            ReportProgress(0);
+            try
+            {
+                if (File.Exists(sourceFile))
+                {
+                    string directory = Path.GetDirectoryName(destinationFile);
+                    if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+                    File.Copy(sourceFile, destinationFile, true);
+                }
+            }
+            catch (Exception e)
+            {
+                if (_Installing) _Installing = false;
+                if (_Decompress) _Decompress = false;
+                if (_Uninstalling) _Uninstalling = false;
+                MessageBox.Show("Đã xảy ra lỗi!\n\n" + e, "Thông báo");
+                ReportStatus("Chưa rõ");
+            }
+        }
+
+        private void Replace_Text(string sourceFile, string translationFile)
+        {
+            if (!_Installing && !_Decompress) return;
+            try
+            {
+                ReportStatus($"Đang sao chép bản dịch");
+                Dictionary<string, string> dictionary = new Dictionary<string, string>();
+                using (StreamReader reader = new StreamReader(translationFile))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string[] content = reader.ReadLine().Split(new string[] { "\t" }, StringSplitOptions.None);
+                        if (!string.IsNullOrWhiteSpace(content[0]) && !string.IsNullOrWhiteSpace(content[1]))
+                        {
+                            try
+                            {
+                                dictionary.Add(content[0], content[1]);
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+                    }
+                }
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    using (StreamWriter temp = new StreamWriter(stream, Encoding.Unicode))
+                    {
+                        using (StreamReader reader = new StreamReader(sourceFile))
+                        {
+                            while (!reader.EndOfStream)
+                            {
+                                string[] content = reader.ReadLine().Split(new string[] { "\t" }, StringSplitOptions.None);
+                                string value;
+                                if (!string.IsNullOrWhiteSpace(content[5]) && dictionary.TryGetValue(content[5], out value))
+                                {
+                                    Write_Log($"{content[5]} => {value}");
+                                    content[5] = value;
+                                }
+                                temp.WriteLine(string.Join("\t", content));
+                            }
+                        }
+                        temp.Flush();
+                        FileStream writeStream = new FileStream(sourceFile, FileMode.Create);
+                        BinaryWriter writeBinary = new BinaryWriter(writeStream);
+                        writeBinary.Write(stream.ToArray());
+                        writeBinary.Close();
+                    }
+                }
+                dictionary.Clear();
+            } 
+            catch (Exception e)
+            {
+                MessageBox.Show("Đã xảy ra lỗi!\n\n" + e, "Thông báo");
+                if (_Installing) _Installing = false;
+                if (_Decompress) _Decompress = false;
+                ReportStatus("Chưa rõ");
+            }
+        }
+
+        private void Remove_Duplicate(string file)
+        {
+            if (!_Installing && !_Decompress) return;
+            try
+            {
+                int total = File.ReadAllLines(file).Count();
+                int count = 0;
+                List<string> lines = new List<string>();
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    using (var temp = new StreamWriter(stream, Encoding.Unicode))
+                    {
+                        using (StreamReader reader = new StreamReader(file))
+                        {
+                            while (!reader.EndOfStream)
+                            {
+                                count++;
+                                ReportStatus($"Đang kiểm tra những câu bị trùng ({count}/{total} dòng)");
+                                string[] content = reader.ReadLine().Split(new string[] { "\t" }, StringSplitOptions.None);
+                                if (!string.IsNullOrWhiteSpace(content[5]) && !lines.Contains(content[5]) && content[5] != "<null>")
+                                {
+                                    temp.WriteLine($"{content[5]}\t");
+                                    lines.Add(content[5]);
+                                }
+                            }
+                        }
+                        temp.Flush();
+                        FileStream writeStream = new FileStream(file, FileMode.Create);
+                        BinaryWriter writeBinary = new BinaryWriter(writeStream);
+                        writeBinary.Write(stream.ToArray());
+                        writeBinary.Close();
+                    }
+                }
+                lines.Clear();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Đã xảy ra lỗi!\n\n" + e, "Thông báo");
+                if (_Installing) _Installing = false;
+                if (_Decompress) _Decompress = false;
+                ReportStatus("Chưa rõ");
+            }
+        }
+        private MemoryStream decompress(string file)
+        {
+            ReportStatus("Đang giải nén");
+            MemoryStream stream = new MemoryStream();
+            try
+            {
+                using (var input = File.OpenRead(file))
+                {
+                    input.Seek(6, SeekOrigin.Current);
+                    using (var deflateStream = new DeflateStream(input, CompressionMode.Decompress, true))
+                    {
+                        deflateStream.CopyTo(stream);
+                    }
+                } 
+            }
+            catch
+            {
+                ReportStatus("Đã xảy ra lỗi");
+                if (_Installing) _Installing = false;
+                if (_Decompress) _Decompress = false;
+            }
+            return stream;
+        }
+
+        private void compress(MemoryStream stream, string file)
+        {
+            if (!_Installing) return;
+            stream.Position = 0;
+            byte[] input = stream.ToArray();
+            byte[] size = BitConverter.GetBytes(Convert.ToUInt32(input.Length));
+            ReportStatus($"Đang nén ({Convert.ToUInt32(input.Length)} byte)");
+            Deflater compressor = new Deflater();
+            compressor.SetLevel(Deflater.BEST_SPEED);
+            compressor.SetInput(input);
+            compressor.Finish();
+            MemoryStream bos = new MemoryStream(input.Length);
+            byte[] buf = new byte[1024];
+            while (!compressor.IsFinished)
+            {
+                int count = compressor.Deflate(buf);
+                bos.Write(buf, 0, count);
+            }
+            try
+            {
+                string directory = Path.GetDirectoryName(file);
+                string filename = Path.GetFileNameWithoutExtension(file);
+                FileStream writeStream = new FileStream($"{directory}\\{filename}.loc", FileMode.Create);
+                BinaryWriter writeBinary = new BinaryWriter(writeStream);
+                writeBinary.Write(size);
+                writeBinary.Write(bos.ToArray());
+                writeBinary.Close();
+            } catch (Exception e)
+            {
+                MessageBox.Show("Đã xảy ra lỗi!\n\n" + e, "Thông báo");
+                if (_Installing) _Installing = false;
+                ReportStatus("Chưa rõ");
+            }
+        }
+
+        private void decrypt(MemoryStream stream, string decryptFile)
+        {
+            if (!_Installing && !_Decompress) return;
+            try
+            {
+                stream.Position = 0;
+                using (var reader = new BinaryReader(stream))
+                {
+                    double total = reader.BaseStream.Length;
+                    double position = 0;
+                    ReportStatus($"Đang giải mã ({total} byte)");
+                    using (var output = new StreamWriter(decryptFile, false, Encoding.Unicode))
+                    {
+                        
+                        while (reader.BaseStream.Position != reader.BaseStream.Length)
+                        {
+                            UInt32 strSize = reader.ReadUInt32();
+                            UInt32 strType = reader.ReadUInt32();
+                            UInt32 strID1 = reader.ReadUInt32();
+                            UInt16 strID2 = reader.ReadUInt16();
+                            byte strID3 = reader.ReadByte();
+                            byte strID4 = reader.ReadByte();
+                            string str = Encoding.Unicode.GetString(reader.ReadBytes(Convert.ToInt32(strSize * 2))).Replace("\n", "<lf>");
+                            reader.ReadBytes(4);
+                            output.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", strType, strID1, strID2, strID3, strID4, str);
+                            position = reader.BaseStream.Position;
+                        }
+                    }
+                }
+            } 
+            catch (Exception e)
+            {
+                MessageBox.Show("Đã xảy ra lỗi!\n\n" + e, "Thông báo");
+                if (_Installing) _Installing = false;
+                if (_Decompress) _Decompress = false;
+                ReportStatus("Chưa rõ");
+            }
+        }
+
+        private MemoryStream encrypt(string file)
+        {
+            MemoryStream stream = new MemoryStream();
+            try
+            {
+                ReportStatus($"Đang mã hóa");
+                using (var reader = new StreamReader(file))
+                {
+                    BinaryWriter writeBinary = new BinaryWriter(stream);
+                    byte[] zeroes = { (byte)0, (byte)0, (byte)0, (byte)0 };
+                    while (!reader.EndOfStream)
+                    {
+                        string[] content = reader.ReadLine().Split(new string[] { "\t" }, StringSplitOptions.None);
+                        byte[] strType = BitConverter.GetBytes(Convert.ToUInt32(content[0]));
+                        byte[] strID1 = BitConverter.GetBytes(Convert.ToUInt32(content[1]));
+                        byte[] strID2 = BitConverter.GetBytes(Convert.ToUInt16(content[2]));
+                        byte strID3 = Convert.ToByte(content[3]);
+                        byte strID4 = Convert.ToByte(content[4]);
+                        string str = content[5].Replace("<lf>", "\n");
+                        byte[] strBytes = Encoding.Unicode.GetBytes(str);
+                        byte[] strSize = BitConverter.GetBytes(str.Length);
+                        writeBinary.Write(strSize);
+                        writeBinary.Write(strType);
+                        writeBinary.Write(strID1);
+                        writeBinary.Write(strID2);
+                        writeBinary.Write(strID3);
+                        writeBinary.Write(strID4);
+                        writeBinary.Write(strBytes);
+                        writeBinary.Write(zeroes);
+                    }
+                    reader.Close();
+                }
+            } 
+            catch
+            {
+                ReportStatus("Đã xảy ra lỗi");
+                if (_Installing) _Installing = false;
+                if (_Decompress) _Decompress = false;
+            }
+            return stream;
+        }
+        private void linkGithub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/lehieugch68/BDO-Translation-Tool");
+        }
+        private void linkVHG_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://viethoagame.com/threads/pc-black-desert-online-viet-hoa.222/");
+        }
+    }
+
+
+}
